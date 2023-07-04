fdf41a68f71556a2821edc3278460b53568cce9f
PTE Master Developer <developer@masterpte.com.au>
2023-07-04 22:58:47 +1000

    styling

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -14,25 +14,35 @@ const VerticalFeatures = () => (
       details={
         <Fragment>
           <p className="py-2">
-            <span className="font-bold">
-              1.Access to a larger pool of qualified workers:
+            <span className="font-bold text-lg text-primary-300">
+              1. Access to a larger pool of qualified workers:
             </span>{' '}
-            By using our platform, child care centers can access a larger pool
-            pool of qualified and vetted workers who are available for casual or
-            part-time work. We&apos;ll do all the screening for you to save your
-            time and resources needed for recruitment.
+            <p>
+              By using our platform, child care centers can access a larger pool
+              pool of qualified and vetted workers who are available for casual
+              or part-time work. We&apos;ll do all the screening for you to save
+              your time and resources needed for recruitment.
+            </p>
           </p>
           <p className="py-2">
-            <span className="font-bold">2.Real-time notifications:</span>{' '}
-            SpaceShift&apos;s real-time notifications can help child care
-            centers centers to quickly and easily find workers when they need
-            them.
+            <span className="font-bold text-lg text-primary-300">
+              2. Real-time notifications:
+            </span>{' '}
+            <p>
+              SpaceShift&apos;s real-time notifications can help child care
+              centers centers to quickly and easily find workers when they need
+              them.
+            </p>
           </p>
           <p className="py-2">
-            <span className="font-bold">3.Reduced administrative burden:</span>{' '}
-            Our platform could help to reduce the the administrative burden of
-            managing casual jobs by allowing easy posting of availale positions,
-            viewing and accepting applicant&apos;s profiles.
+            <span className="font-bold text-lg text-primary-300">
+              3. Reduced administrative burden:
+            </span>{' '}
+            <p>
+              Our platform could help to reduce the the administrative burden of
+              managing casual jobs by allowing easy posting of availale
+              positions, viewing and accepting applicant&apos;s profiles.
+            </p>
           </p>
         </Fragment>
       }
@@ -45,29 +55,53 @@ const VerticalFeatures = () => (
       details={
         <Fragment>
           <p className="py-2">
-            <span className="font-bold">1.Flexibility:</span> Casual workers can
-            choose their preferred working hours and days according to their
-            schedule.
+            <span className="font-bold text-lg text-primary-300">
+              1. Flexibility:
+            </span>{' '}
+            <p>
+              Casual workers can choose their preferred working hours and days
+              according to their schedule.
+            </p>
           </p>
           <p className="py-2">
-            <span className="font-bold">2.Variety:</span> Our marketplace
-            provides access to a wide range of child care centers, allowing
-            workers to gain experience in different environments.
+            <span className="font-bold text-lg text-primary-300">
+              2. Variety:
+            </span>{' '}
+            <p>
+              Our marketplace provides access to a wide range of child care
+              centers, allowing workers to gain experience in different
+              environments.
+            </p>
           </p>
           <p className="py-2">
-            <span className="font-bold">3.Convenience:</span> With our real-time
-            notification system, workers can receive alerts about available
-            shifts and apply for them instantly from their mobile devices.
+            <span className="font-bold text-lg text-primary-300">
+              3. Convenience:
+            </span>{' '}
+            <p>
+              With our real-time notification system, workers can receive alerts
+              about available shifts and apply for them instantly from their
+              mobile devices.
+            </p>
           </p>
           <p className="py-2">
-            <span className="font-bold">4.Professional development:</span>{' '}
-            Casual workers can learn new skills and gain experience in different
-            aspects of child care through working in different centers.
+            <span className="font-bold text-lg text-primary-300">
+              4. Professional development:
+            </span>{' '}
+            <p>
+              Casual workers can learn new skills and gain experience in
+              different aspects of child care through working in different
+              centers.
+            </p>
           </p>
           <p className="py-2">
-            <span className="font-bold">5.Build connections:</span> By working
-            in multiple centers, workers can network with colleagues and
-            potential employers, which can lead to future job opportunities.
+            <span className="font-bold text-lg text-primary-300">
+              5. Build connections:
+            </span>{' '}
+            <p>
+              By working in multiple centers, workers can network with
+              colleagues and potential employers, which can lead to future job
+              opportunities.
+            </p>
           </p>
         </Fragment>
       }
